8d52076121293c46b76d88cc3cbea8e371a0de1d
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2019-03-02 16:42:46 +0530

    updated

--- javascript.js
+++
@@ -1,6 +0,0 @@
-<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-	 <script type="text/javascript">
-	 	$(".input").focus(function() {
-	 		$(this).parent().addClass("focus");
-	 	})
-	 </script>
