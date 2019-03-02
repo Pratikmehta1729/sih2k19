3f9b7b83a6159fbc010541e0b821105ada819078
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2019-03-02 09:12:02 +0530

    added index page

---
+++ javascript.js
@@ -0,0 +1,6 @@
+<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+	 <script type="text/javascript">
+	 	$(".input").focus(function() {
+	 		$(this).parent().addClass("focus");
+	 	})
+	 </script>
